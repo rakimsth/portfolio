beca397d9c83b5e6e40a45576ca96d7883fc0cf8
Raktim Shrestha <raktim@rumsan.com>
2022-10-25 04:47:57 +0545

    footer fix

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
           <Stack direction="row" spacing={0.5} sx={{ pl: 1 }}>
             <Box
               component={Link}
-              href="https://github.com/noworneverev/react-vscode-portfolio"
+              href="https://github.com/rakimsth/portfolio"
               underline="none"
               color="white"
               target="_blank"
